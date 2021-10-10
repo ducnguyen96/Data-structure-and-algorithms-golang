d236e54de6d42014c606ecbe4a6ae863888aed61
ducnguyen96 <levinguyen.dl@gmail.com>
2021-10-11 03:07:53 +0700

    add merge sort resolved by divide and conquer

---
+++ Algorithms/Sorting/Merge Sort/README.md
@@ -0,0 +1,10 @@
+## Merge Sort
+![](merge_sort.png)
+
+### Divide
+We divide the problem into small independent pieces that easy to resolve
+![](merge_sort_divide.png)
+
+### Conquer
+Resolve those small pieces and merge them together
+![](merge_sort_conquer.png)
--- Algorithms/Sorting/Merge Sort/main.go
+++ Algorithms/Sorting/Merge Sort/main.go
@@ -1,1 +1,50 @@
 package main
+
+import "fmt"
+
+func mergeSort(nums []int) []int {
+	l := len(nums)
+	if l == 1 {
+		return nums
+	} else {
+		// divide
+		mid := (l-1)/2
+		left := mergeSort(nums[:mid+1])
+		right := mergeSort(nums[mid+1:])
+
+		// conquer
+		merge := make([]int, l)
+		lengthLeft := len(left)
+		lengthRight := len(right)
+		currentLeftIndex := 0
+		currentRightIndex := 0
+		currentResultIndex := 0
+
+		// loop through all elements of the two array, compare pair by pair and assign them to the result
+		for currentLeftIndex <= lengthLeft - 1 || currentRightIndex <= lengthRight - 1 {
+			if currentLeftIndex > lengthLeft - 1{
+				merge[currentResultIndex] = right[currentRightIndex]
+				currentRightIndex++
+				currentResultIndex++
+			} else if currentRightIndex > lengthRight - 1{
+				merge[currentResultIndex] = left[currentLeftIndex]
+				currentLeftIndex++
+				currentResultIndex++
+			} else if left[currentLeftIndex] < right[currentRightIndex]{
+				merge[currentResultIndex] = left[currentLeftIndex]
+				currentLeftIndex++
+				currentResultIndex++
+			} else {
+				merge[currentResultIndex] = right[currentRightIndex]
+				currentRightIndex++
+				currentResultIndex++
+			}
+		}
+		return merge
+	}
+}
+
+func main() {
+	nums := []int{6, 5, 3, 1, 8, 7, 2, 4}
+	fmt.Println(mergeSort(nums))
+}
